1f4722c321117a6db797e3170f9fc28532d629f7
harshil412 <harshilj.patel65@gmail.com>
2024-07-09 23:18:23 +0530

    1 - post route and controller added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
+import postRoutes from './routes/post.route.js';
 import connectMongoDB from './db/connectMongoDB.js';
 import cookieParser from 'cookie-parser'
 import { v2 as cloudinary } from 'cloudinary'
@@ -23,6 +24,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
