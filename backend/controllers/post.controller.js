e092c955fde4a6ae887ebe7f8f8a2c934cfb3dd3
harshil412 <harshilj.patel65@gmail.com>
2024-07-13 11:37:00 +0530

    3 - like,comment post

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -96,7 +96,9 @@ export const likeUnlikePost = async (req, res) => {
             //unlike post
             await Post.updateOne({ _id: postId}, { $pull : {likes: userId} });
             await User.updateOne({ _id: userId}, { $pull : {likedPosts: postId} });
-            res.status(200).json({message : "Post unlinked successfully"})
+
+            const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString());
+			res.status(200).json(updatedLikes);
         } else {
             post.likes.push(userId);
             await User.updateOne({ _id: userId}, { $push : {likedPosts: postId} });
@@ -109,7 +111,8 @@ export const likeUnlikePost = async (req, res) => {
             });
 
             await notification.save();
-            res.status(200).json({message : "Post linked successfully"});
+            const updatedLikes = post.likes;
+			res.status(200).json(updatedLikes);
         }
     } catch (error) {
         res.status(500).json({ error : error.message });
